1a655893997d61fd8076c01c277b15ea4bf03fff
Shuchita Gupta <shuchi2133@gmail.com>
2024-12-04 18:58:54 +0530

    Calculated wages till a condition of total days is reached

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,28 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello and Welcome to Employee Wage Computation Program");
     	Employee e=new Employee("Ram",123);
-    	int countPresent=0;
+    	e.normalWorkDay=8;
     	
-    	for(int n=0;n<20;n++) {
+    	int countPresent=0;
+    	int empWage=0;
+    	
+    	while(countPresent!=20) {
     		Random r=new Random();
         	int i=(r.nextInt(1000))%2;	
         	
         	switch(i) {
-        	case 1->{e.attendance(true); countPresent++;}
+        	case 1->{
+        		e.attendance(true); 
+        		countPresent++; 
+        		empWage= empWage+ (e.normalWorkDay*e.salPerHour);
+        	}
         	default->e.attendance(false);
         	}
+        	
     	}
     
-    	e.normalWorkDay=8;
+  	
         System.out.println("Employee present "+countPresent +" days(out of 20)."
-        		+ "Total salary for month is "+ countPresent*e.calculateWage());
+        		+ "Total salary for month is "+empWage);
     }
 }
